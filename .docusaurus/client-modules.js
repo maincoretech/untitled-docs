312bebf4f21eeb8d400902dba1f060166ed89ad9
shiftz300 <kasetry300@outlook.com>
2024-02-18 14:32:26 +0000

    init docusaurus

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/home/coder/project/untitled-docs/node_modules/infima/dist/css/default/default.css"),
+  require("/home/coder/project/untitled-docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/home/coder/project/untitled-docs/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/home/coder/project/untitled-docs/src/css/custom.css"),
+];
