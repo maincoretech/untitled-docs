b28468ffd9ed20d6c452214330470a2a90f57795
shiftz300 <kasetry300@outlook.com>
2024-06-23 04:49:59 +0800

    none

--- .docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require("/home/coder/project/untitled-docs/node_modules/infima/dist/css/default/default.css"),
-  require("/home/coder/project/untitled-docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/home/coder/project/untitled-docs/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/home/coder/project/untitled-docs/src/css/custom.css"),
-  require("/home/coder/project/untitled-docs/node_modules/docusaurus-plugin-image-zoom/lib/zoom"),
-];
